28428746445f77257547e421f77e3fe0990cc51c
Kevin Sylvestre <kevin@ksylvest.com>
2023-07-18 13:40:19 -0700

    Sample redirect for mobile / tablet

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const APP_URL =
+  "https://apps.apple.com/app/apple-store/id1512673045?pt=80402&ct=GetHearts&mt=8";
+
+const FALLBACK_URL = "https://www.neverboredstudios.com/gethearts";
+
+export function middleware(request: NextRequest) {
+  if (request.url.includes("direct.gethearts.com")) return;
+
+  const ua = request.headers.get("user-agent");
+
+  if (ua?.includes("Macintosh;") || ua?.includes("iPad;"))
+    return NextResponse.redirect(APP_URL);
+
+  return NextResponse.redirect(FALLBACK_URL);
+}
